6dd6fe95193bbdb5242908e2bc31f0290b2f7cda
ricardopatriciodossantos <ricardo.dossantos@amadeus.com>
2026-06-25 12:58:48 +0100

    fix: switch from Full to MRZRFID database (enables coexistence with RFID SDK)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vbmobile/AMADocModeliOS", .upToNextMinor(from: "2.0.0")),
         .package(url: "https://github.com/regulaforensics/DocumentReader-Swift-Package", from: "8.3.0"),
-        .package(url: "https://github.com/regulaforensics/DocumentReaderFull-Swift-Package", from: "8.3.0"),
+        .package(url: "https://github.com/regulaforensics/DocumentReaderMRZRFID-Swift-Package", from: "8.3.0"),
     ],
     targets: [
         .binaryTarget(
@@ -29,7 +29,7 @@ let package = Package(
                 .target(name: "AMAMRZOCRReadRegula"),
                 .product(name: "AMADocModeliOS", package: "AMADocModeliOS"),
                 .product(name: "DocumentReader", package: "DocumentReader-Swift-Package"),
-                .product(name: "Full", package: "DocumentReaderFull-Swift-Package"),
+                .product(name: "MRZRFID", package: "DocumentReaderMRZRFID-Swift-Package"),
             ],
             path: "Sources",
             sources: ["AMAMRZOCRReadRegula.swift"]
