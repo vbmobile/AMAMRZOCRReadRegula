166ad169718d46424003411e509a94e7ae4e97d4
ricardopatriciodossantos <ricardo.dossantos@amadeus.com>
2026-07-06 20:38:08 +0100

    AMAMRZOCRReadRegula 1.0.1
    Adopt renamed AMA repos (AMADocModel 3.0.1, AMANetworkClient 6.0.1),
    point binary target at AMAMRZOCRReadRegula-1.0.1.zip.

--- Package.swift
+++ Package.swift
@@ -13,16 +13,16 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/vbmobile/AMADocModeliOS", .upToNextMinor(from: "2.0.0")),
-        .package(url: "https://github.com/vbmobile/VBNetworkClient", exact: "5.1.1"),
+        .package(url: "https://github.com/vbmobile/AMADocModel", .upToNextMinor(from: "3.0.1")),
+        .package(url: "https://github.com/vbmobile/AMANetworkClient", exact: "6.0.1"),
         .package(url: "https://github.com/regulaforensics/DocumentReader-Swift-Package", from: "8.3.0"),
         .package(url: "https://github.com/regulaforensics/DocumentReaderMRZRFID-Swift-Package", from: "8.3.0"),
     ],
     targets: [
         .binaryTarget(
             name: "AMAMRZOCRReadRegula",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMAMRZOCRReadRegula/AMAMRZOCRReadRegula-1.0.0-rc03.zip",
-            checksum: "c529fb1ca04a3a002a0de3d41bd3f78825ece7eb69a0f3b1e1445ad0ba397bbe"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMAMRZOCRReadRegula/AMAMRZOCRReadRegula-1.0.1.zip",
+            checksum: "a767ec8c56a50b3a43ee3deebc179e2f7b445fa92cedd592bac4318a2ba23bda"
         )
     ]
 )
